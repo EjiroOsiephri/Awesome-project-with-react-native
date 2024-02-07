bfc24ad4a055076d6618ed14371eb19f6b87889f
EjiroOsiephri <osiephriejiro765@gmail.com>
2024-02-07 08:41:39 +0100

    Add: Using Images with react native

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Image, Text, View } from "react-native";
+
+const PlaceholderImage = require("./assets/images/b.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={textStyles.container}>
+        This is my first react native project
+      </Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 340,
+    height: 440,
+    borderRadius: 14,
+  },
+});
+
+const textStyles = StyleSheet.create({
+  container: {
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
   },
 });
